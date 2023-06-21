40a8815929a0a298398fbf8d8a6cb6e0408a2205
amir <khazaeli7amir@gmail.com>
2023-06-21 19:12:41 +0330

    Add typeWriter component to  searchbox component

--- src/Components/SearchBox/SearchBox.jsx
+++ src/Components/SearchBox/SearchBox.jsx
@@ -5,14 +5,39 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { Container } from 'react-bootstrap'
 import TopHeader from '../TopHeader/TopHeader'
 import NavbarComponent from '../Navbar/Navbar'
-export default function SearchBox({onSearch}) {
+import TypewriterComponent from 'typewriter-effect'
+export default function SearchBox({ onSearch }) {
   return (
     <>
       <div className="search-Box">
         <TopHeader />
         <NavbarComponent />
         <Container className='search-box-container' fluid='lg'>
-          <h1>به آکادمی سبزلرن خوش آمدید</h1>
+          <h1>
+            <TypewriterComponent 
+            onInit={(typeWriter)=> {
+              typeWriter
+              .typeString("ما به هر قیمتی دوره اموزشی تولید نمیکنیم!")
+              .start()
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString('به آکادمی سبزلرن خوش آمدید')
+              .start()
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString('طراحی شده توسط امیر خزایلی')
+              .start()
+              .pauseFor(2000)
+              .deleteAll()
+            }}
+            options={{
+              loop: true
+            }
+            }
+            />
+
+          </h1>
+          <h2>آموزش برنامه نویسی در سایت سبزلرن</h2>
           <div className='search-box-input'>
             <input onChange={(e) => onSearch(e)} type="text" placeholder='جستجو..' />
             <FontAwesomeIcon icon={faMagnifyingGlass} />
